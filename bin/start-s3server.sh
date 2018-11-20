85973dc0626cd721dd20eeddff7c3ebd7d72bde9
Howard Ding <hding2@illinois.edu>
2018-11-20 16:37:00 -0600

    Don't need these scripts

--- bin/start-s3server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Starting server"
-docker pull scality/s3server:mem-latest
-docker restart $S3_SERVER_DOCKER_NAME
--- bin/stop-s3server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker stop $S3_SERVER_DOCKER_NAME
