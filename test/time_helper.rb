e8fe5cf317c8c343dc3492e9126e62b1fbfa4379
Howard Ding <hding2@illinois.edu>
2018-11-21 10:01:41 -0600

    fix test

--- test/time_helper.rb
+++ test/time_helper.rb
@@ -1,7 +1,13 @@
 module TimeHelper
 
+  module_function
+
   def time_equal(time_1, time_2)
     time_1.to_f == time_2.to_f
+  end
+
+  def time_less_than_or_eq(time_1, time_2)
+    time_1.to_f <= time_2.to_f
   end
 
 end
